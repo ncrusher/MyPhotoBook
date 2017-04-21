753e62cb9b3f5a7bdf743248d9416c4c7058d302
Khai Le <khaikhai@Khais-MBP.attlocal.net>
2017-04-21 12:16:21 -0700

    Completed setting up firebase integration

---
+++ Pods/Target Support Files/Pods-MyPhotoBookTests/Pods-MyPhotoBookTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MyPhotoBookTests : NSObject
+@end
+@implementation PodsDummy_Pods_MyPhotoBookTests
+@end
---
+++ Pods/Target Support Files/Pods-MyPhotoBookUITests/Pods-MyPhotoBookUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MyPhotoBookUITests : NSObject
+@end
+@implementation PodsDummy_Pods_MyPhotoBookUITests
+@end
